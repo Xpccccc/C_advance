8bb951df27fb1ba76c08e60cc16881db9fbcc346
Xp <1014647664@qq.com>
2023-04-16 11:26:04 +0800

    memcpy的用法

--- day_13/demo_12/main.c
+++ day_13/demo_12/main.c
@@ -4,6 +4,7 @@
 int main() {
     int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int arr2[5] = {0};
+    // memcpy(arr1+2, arr1, 20)在内存重叠的时候，使用memcpy可能出现意想不到的效果
     memcpy(arr2, arr1, 20);//arr1拷贝20个字节到arr2
     for (int i = 0; i < 5; ++i) {
         printf("%d ",arr2[i]);
