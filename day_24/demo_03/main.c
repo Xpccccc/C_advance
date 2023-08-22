9686a91a3c4d7127424db394ff9d0c110a27303f
Xpccccc <1014647664@qq.com>
2023-08-23 00:51:01 +0800

    JZ65 不用加减乘除做加法

--- day_24/demo_03/main.c
+++ day_24/demo_03/main.c
@@ -2,7 +2,9 @@
 //JZ65 不用加减乘除做加法
 int Add(int num1, int num2) {
     // write code here
+    //不进位的情况
     if (num2 == 0)
         return num1;
-    return Add(num1 ^ num2, (num1 & num2) << 1);
+    //进位的情况
+    return Add(num1 ^ num2, (num1 & num2) << 1);//num1 ^ num2得出除掉进位以外的位的和，(num1 & num2) << 1得出进位或者不进位的情况
 }
